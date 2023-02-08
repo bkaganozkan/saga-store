90d0838bf4b9811900a337bae778d00273e6f3b6
oguzhanylcnkaya <Oguzhanyalcinkayaa@gmail.com>
2023-02-08 19:20:23 +0300

    lostark page redesigned

--- src/components/LostArk/LostArk.vue
+++ src/components/LostArk/LostArk.vue
@@ -1,240 +1,60 @@
 <template>
-  <div class="main">
+  <v-main>
     <Header />
-    <v-container class="main-container">
-      <v-row class="main-row">
-        <v-col cols="3" class="lostark-img">
-          <img src="@/assets/images/lostark.jpeg" alt="LostArk" />
-        </v-col>
-
-        <v-col class="content">
-          <v-row>
-            <v-col class="content-left">
-              <div>
-                <p class="content-left-text">LOST ARK EUC (ALL SERVER)</p>
-                <p class="content-left-text">1000 Gold</p>
-                <p class="content-left-icon">
-                  <v-icon large color="green darken-2">
-                    mdi-check-circle
-                  </v-icon>
-                  Available
-                </p>
-              </div>
-            </v-col>
-            <v-col class="content-right">
-              <v-select
-                color="#9C27B0"
-                :items="getServerLocations"
-                label="Server Location"
-                outlined
-                item-text="label"
-                v-model="selectedServerLocation"
-              ></v-select>
-
-              <v-select
-                color="#9C27B0"
-                :items="getServerInfo"
-                label="Server Name"
-                return-object
-                outlined
-                item-text="name"
-                v-model="selectedServer"
-                @change="changedServer()"
-              ></v-select>
-
-              <v-text-field
-                class="quantity-input"
-                v-model="quantity"
-                color="#9C27B0"
-                label="Fiyat Adet"
-                outlined
-                @change="changedQuantity()"
-              ></v-text-field>
-              <p>
-                <v-icon large> mdi-information-outline </v-icon>
-                {{ totalAmount }} EUR
-              </p>
-            </v-col>
-          </v-row>
-        </v-col>
-
-        <v-col cols="3">
-          <div class="left-area">
-            <h6 class="left-area-title">PLEASE READ THE WARNING!</h6>
-            <p class="left-area-content">
-              Delivery time of the order " 15 Minutes " Delivery information
-              after purchase My Orders from the relevant order page DETAIL You
-              can learn by clicking the button. After receiving your Game Money,
-              do not return it to anyone. In the game Anathema Yang He
-              definitely won't talk to you.. Get information by contacting live
-              support for any questions or problems that may arise during
-              delivery. LOSSES CAUSED BY THESE TYPE OF ERRORS Anathema Yang NOT
-              RESPONSIBLE.
-            </p>
-          </div>
-          <div class="discord-area">
-            <p>BUY NOW Via Discord</p>
-            <v-btn
-              style="justify-self: end"
-              href="https://discord.gg/Q4BkmGME"
-              target="_blank"
-              depressed
-              plain
-              class="discord-btn"
-            >
-              <v-img
-                src="@/assets/images/discord.png"
-                alt="Discord"
-                height="100"
-                width="100"
-              >
-              </v-img>
-            </v-btn>
-          </div>
-        </v-col>
+    <div class="main-container">
+      <v-row class="card-container">
+        <v-col cols="4" class="pa-0 left-container"> </v-col>
+        <LostArkServers />
+        <RightArea />
       </v-row>
-    </v-container>
-  </div>
+    </div>
+  </v-main>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
 import Header from "@/components/Header.vue";
+import RightArea from "./lostArk-rightArea.vue";
+import LostArkServers from "./lostArk-servers.vue";
 export default {
-  data: () => ({
-    selectedServer: null,
-    quantity: null,
-    totalAmount: 0.0,
-    selectedServerLocation: "all",
-  }),
-  computed: {
-    ...mapGetters(["getServerLocations"]),
-    getServerInfo() {
-      return this.$store.getters.getServerInfo(this.selectedServerLocation);
-    },
-  },
-  methods: {
-    changedServer() {
-      if (this.quantity == null) {
-        this.quantity = 1;
-      }
-      this.calculateTotalAmount();
-    },
-    changedQuantity() {
-      this.calculateTotalAmount();
-    },
-    calculateTotalAmount() {
-      this.totalAmount = this.selectedServer.price * this.quantity;
-    },
-  },
   components: {
     Header,
+    RightArea,
+    LostArkServers
   },
 };
 </script>
 
-<style lang="scss">
-$green-color: #388e3c;
-$left-area-bgColor : #ffe699;
-$left-area-textColor: #977e38;
+<style lang="scss" scoped>
 
-@function toRem($value) {
-    $remValue: ($value / 16) + rem; 
-    @return $remValue;
-}
-
-input {
-  margin: 0 !important;
-}
-
-.main {
-  height: 100%;
-  padding: 0;
-  margin: 0;
-
-  &-container{
-    margin-top: toRem(80);
-    height: calc(100% - toRem(80));
-  }
-
-  &-row {
-    height: 100%;
-  }
-}
-
-.lostark-img {
-  & img {
-    height: 83%;
-    width: 100%;
-  }
-}
-.content {
+.main-container {
+  position: relative;
   display: flex;
-  align-items: center;
-
-  &-right {
-    p {
-      display: flex;
-      justify-content: end;
-      align-items: center;
-
-      i {
-        margin-right: 1rem;
-      }
-    }
-  }
-
-  &-left {
-    display: flex;
-    align-items: center;
-
-    &-text {
-      margin: 0 !important;
-      font-weight: 400;
-    }
-
-    &-icon {
-      margin-top: 3rem;
-      color: $green-color;
-      font-weight: bold;
-      font-size: 1.2rem;
-    }
-  }
-}
-.left-area {
-  background: $left-area-bgColor;
-  height: 80%;
-  padding: 1rem;
-  color: $left-area-textColor;
-
-  &-title{
-    margin-bottom: 1rem;
-  }
-
-  &-content{
-    text-align: justify;
-    font-size: toRem(16);
-  }
-}
-
-.discord-area {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  height: 20%;
   justify-content: center;
+  align-items: center;
+  margin-top: 3em;
+  background: rgba(#1e1e1e, 1);
+  height: 100vh;
 
-  & p {
-    font-weight: 600;
-    font-size: 1.2rem;
-  }
-
-  & .discord-btn {
-    margin-top: 1rem;
-  }
-
-  & img {
-    width: 35%;
+  & .card-container {
+    position: absolute;
+    width: 70vw;
+    height: calc(100vh - 190px);
+    border-radius: 41px;
+    background: rgba($color: #f8f8f8, $alpha: 1);
+    -webkit-box-shadow: 0px 0px 10px 0px rgba(255, 255, 255, 0.3);
+    -moz-box-shadow: 0px 0px 10px 0px rgba(255, 255, 255, 0.3);
+    box-shadow: 0px 0px 10px 0px rgba(255, 255, 255, 0.3);
   }
 }
+
+.left-container {
+  background: url(@/assets/images/lostarkintro.jpeg);
+  border-radius: 41px 0px 0px 41px;
+  width: 325px;
+  height: 100%;
+  background-repeat: no-repeat;
+  filter: blur(1px);
+  background-size: cover;
+}
+
 </style>
---
+++ src/components/LostArk/lostArk-servers.vue
@@ -0,0 +1,140 @@
+<template>
+  <v-col cols="4">
+
+    <p class="lostark-title">LOST ARK ALL SERVER <span class="gold">GOLD</span> SALE</p>
+
+    <label class="label-text" for="serverLocation">Server Location</label>
+    <v-select
+      class="select-area"
+      outlined
+      :items="getServerLocations"
+      v-model="selectedServerLocation"
+      item-text="label"
+    ></v-select>
+
+    <label class="label-text" for="serverName">Server Name</label>
+    <v-select
+      class="select-area"
+      :items="getServerInfo"
+      return-object
+      outlined
+      item-text="name"
+      v-model="selectedServer"
+      @change="changedServer()"
+    ></v-select>
+
+    <label class="label-text" for="serverName">Piece (1000g)</label>
+    <v-text-field
+    min="10"
+      class="select-area"
+      v-model="quantity"
+      outlined
+      @change="changedQuantity()"
+    ></v-text-field>
+
+    <label class="label-text" for="serverName">Item Information</label>
+    <v-textarea rows="3" class="select-area" v-model="information" outlined>
+    </v-textarea>
+
+    <v-row justify="space-between" class="px-3 mt-3">
+      <span class="price-text"
+        >Price
+        {{ totalAmount }}
+        €
+      </span>
+
+      <v-btn dark color="#3155A3">Buy Now</v-btn>
+    </v-row>
+  </v-col>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  data: () => ({
+    selectedServer: null,
+    quantity: null,
+    information: null,
+    totalAmount: 0.0,
+    selectedServerLocation: "all",
+  }),
+  computed: {
+    ...mapGetters(["getServerLocations"]),
+    getServerInfo() {
+      return this.$store.getters.getServerInfo(this.selectedServerLocation);
+    },
+  },
+  methods: {
+    changedServer() {
+      if (this.quantity == null) {
+        this.quantity = 1;
+      }
+      this.calculateTotalAmount();
+    },
+    changedQuantity() {
+      this.calculateTotalAmount();
+    },
+    calculateTotalAmount() {
+      this.totalAmount = this.selectedServer.price * this.quantity;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+$text-color: #2D2D2D;
+$gold-color: #FED500;
+
+@function toRem($value) {
+  $remValue: ($value / 16) + rem;
+  @return $remValue;
+}
+
+.v-text-field input{
+  margin: 0 !important;
+}
+
+.v-text-field--outlined > .v-input__control > .v-input__slot{
+  min-height: toRem(40) !important;
+}
+
+.v-text-field__details{
+  display: none !important;
+}
+
+.textarea{
+  margin: 0 0 5px 0 !important;
+}
+
+.lostark-title{
+  color: $text-color;
+  font-weight: 500;
+  text-shadow: 2px 2px #00000040;
+
+  & .gold{
+    color: $gold-color;
+  }
+}
+
+.price-text {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 24px;
+  display: flex;
+  align-items: center;
+  text-shadow: 2px 2px #00000040;
+  color: #2b2b2b;
+}
+
+.label-text {
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 19.36px;
+  color: #5e5e5e;
+}
+
+.select-area {
+  border-radius: 8px;
+}
+</style>
